c3622a4a8849b59ac2f28ffceeb93c2710236d90
Екатерина Рогожина <gulliver159@yandex.ru>
2021-02-10 21:59:18 +0600

    Added creating files with results

--- src/main/java/com/aimconsulting/testing/CSVWriter.java
+++ src/main/java/com/aimconsulting/testing/CSVWriter.java
@@ -1,11 +1,11 @@
 package com.aimconsulting.testing;
 
-import java.util.HashMap;
+import java.io.*;
 import java.util.Set;
 
 public class CSVWriter implements Runnable {
-    private String filename;
-    private Set<String> fileContent;
+    private final String filename;
+    private final Set<String> fileContent;
 
     public CSVWriter(String filename, Set<String> fileContent) {
         this.filename = filename;
@@ -13,5 +13,10 @@ public class CSVWriter implements Runnable {
     }
 
     public void run() {
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(filename))) {
+            bw.write(String.join(CSVFileHandler.SEPARATOR, fileContent) + CSVFileHandler.SEPARATOR);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
