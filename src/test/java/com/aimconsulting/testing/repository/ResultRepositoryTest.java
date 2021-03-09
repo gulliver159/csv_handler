e53922073fedb783d830aadb29ada02e747131e3
Екатерина Рогожина <gulliver159@yandex.ru>
2021-03-09 19:46:35 +0600

    Добавлены тесты для ResultRepository

---
+++ src/test/java/com/aimconsulting/testing/repository/ResultRepositoryTest.java
@@ -0,0 +1,64 @@
+package com.aimconsulting.testing.repository;
+
+import com.aimconsulting.testing.model.Result;
+import com.aimconsulting.testing.repository_interface.ResultWriter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class ResultRepositoryTest {
+
+    private ResultWriter writer;
+
+    @BeforeEach
+    void setUp() {
+        DataSource dataSource = new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:data.sql")
+                .build();
+
+        writer = new ResultRepository(new JdbcTemplate(dataSource));
+    }
+
+    @Test
+    void testCreateResults1() {
+        List<Result> resultList = new ArrayList<>();
+        resultList.add(new Result("id", "0;1;2;"));
+        resultList.add(new Result("version", "1;2;"));
+        resultList.add(new Result("path", "/hello/уточка;/hello/лошадка;/hello/собачка;"));
+
+        writer.createResults(resultList);
+
+        assertAll(
+                () -> assertEquals(resultList.get(0), writer.getResult(resultList.get(0).getName())),
+                () -> assertEquals(resultList.get(1), writer.getResult(resultList.get(1).getName())),
+                () -> assertEquals(resultList.get(2), writer.getResult(resultList.get(2).getName()))
+        );
+    }
+
+    @Test
+    void testCreateResults2() {
+        List<Result> resultList = new ArrayList<>();
+        resultList.add(new Result("id", "0;1;2;3;"));
+        resultList.add(new Result("name", "ричард;жорж;мария;пьер;"));
+        resultList.add(new Result("sex", "м;ж;"));
+
+        writer.createResults(resultList);
+
+        assertAll(
+                () -> assertEquals(resultList.get(0), writer.getResult(resultList.get(0).getName())),
+                () -> assertEquals(resultList.get(1), writer.getResult(resultList.get(1).getName())),
+                () -> assertEquals(resultList.get(2), writer.getResult(resultList.get(2).getName()))
+        );
+    }
+
+}
